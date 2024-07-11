3b6af535df6f1ae2f990c1a1672f30c62f54fc55
Chizi Victor <chiziwokoma@gmail.com>
2024-07-11 22:37:53 +0100

    animation: rossmason page

---
+++ src/routes/animations/rossmason/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import './styles.css';
+	import '@fontsource-variable/bricolage-grotesque';
+	import '@fontsource-variable/jetbrains-mono';
+	import { onMount } from 'svelte';
+	import { animate } from './animate';
+
+	const left = 'CHIZI';
+	const right = 'VICTOR';
+	const leftLetters = left.split('');
+	const rightLetters = right.split('');
+
+	onMount(async () => {
+		const { gsap } = await import('gsap');
+		animate(gsap);
+	});
+</script>
+
+<div class="container">
+	<nav>
+		<div class="logo">
+			<a href=".">{left} {right}</a>
+		</div>
+		<div class="links">
+			<div class="link">
+				<a href=".">Contact Us</a>
+			</div>
+			<div class="link">
+				<a href=".">Menu</a>
+			</div>
+		</div>
+	</nav>
+	<div class="header">
+		{#each leftLetters as letter, idx}
+			<div class="header-item">
+				<div class={`header-item-wrapper h-${idx + 1}`}><h1>{letter}</h1></div>
+			</div>
+		{/each}
+		<div class="preview" />
+		{#each rightLetters as letter, idx}
+			<div class="header-item">
+				<div class={`header-item-wrapper h-${idx + 1}`}><h1>{letter}</h1></div>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/animations/rossmason/animate.ts
@@ -0,0 +1,61 @@
+import { gsap as TGsap } from 'gsap';
+
+export function animate(gsap: typeof TGsap) {
+	const tl = gsap.timeline({ delay: 1 });
+
+	gsap.set('h1', { y: 100 });
+
+	tl.to('h1', {
+		y: 0,
+		duration: 1,
+		ease: 'power3.out',
+		stagger: 0.1
+	});
+
+	tl.to('.header-item', {
+		clipPath: 'none',
+		duration: 0.1
+	});
+
+	tl.from('.preview', {
+		width: '0',
+		margin: '0',
+		duration: 1,
+		ease: 'power3.out'
+	});
+
+	tl.to('.header-item', {
+		translateY: '140%',
+		fontSize: '17vw',
+		duration: 1,
+		ease: 'power3.out'
+	});
+
+	tl.to(
+		'h1',
+		{
+			fontSize: '11vw',
+			duration: 1,
+			ease: 'power3.out'
+		},
+		'<'
+	);
+
+	tl.to(
+		'.preview',
+		{
+			marginLeft: '-2vw',
+			marginRight: '-4vw',
+			width: '30vw',
+			duration: 1,
+			ease: 'power3.out'
+		},
+		'<'
+	);
+
+	tl.from('.logo, .link, footer p', {
+		y: 20,
+		opacity: 0,
+		stagger: 0.1
+	});
+}
