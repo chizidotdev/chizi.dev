673d354d6ccc20a3b7cb41558c9c69580a3e69e7
Chizi Victor <chiziwokoma@gmail.com>
2024-12-10 02:23:46 +0100

    chore: app favicons

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,3 +1,11 @@
+declare module "*.md" {
+  import type { SvelteComponent } from "svelte";
+
+  export default class Comp extends SvelteComponent {}
+
+  export const metadata: Record<string, unknown>;
+}
+
 type Snippet = {
   title: string;
   description: string;
