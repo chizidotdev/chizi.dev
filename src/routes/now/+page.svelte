8f1bb66c6a76bc2fbb32fb012041382afc3cc122
Chizi Victor <chiziwokoma@gmail.com>
2025-03-12 08:56:35 +0100

    feat: replace resume page with now page

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -9,36 +9,27 @@
 </svelte:head>
 
 <section>
-  <Heading variant="h3" class="">Now</Heading>
+  <Heading variant="h3" class="text-muted-foreground">Last updated on Mar 12, 2025.</Heading>
 
-  <div class="mt-4 space-y-6">
-    <Paragraph class="">
-      Running <a class="link" href="https://aidmedium.com" target="_blank">Aidmedium</a>
-      where I assist businesses and startups with software solutions.
-    </Paragraph>
-
-    <Paragraph class="">Spending way too much time tinkering with this site.</Paragraph>
-
-    <Paragraph class="">
-      Journaling and writing are my new hobbies. Nah, that's a lie, I force myself to write. I just
-      try to document my journey and learnings as often as possible.
-    </Paragraph>
-
-    <Heading variant="h3" class=" border-l pl-3">
-      If you need more than two tabs to find the solution, write the answer down somewhere.
-    </Heading>
-
-    <Paragraph class="">
+  <div class="mt-6 space-y-4">
+    <Paragraph>Spending way too much time tinkering with this site.</Paragraph>
+    <Paragraph>Recovering musician.</Paragraph>
+    <Paragraph>
       Reading
       <a class="link" href="https://literal.club/book/jim-edwards-copywriting-secrets-ehc2j">
-        Copywriting Secrets
+        Copywriting Secrets.
       </a>
-      on one end, grinding DSA on the other.
+    </Paragraph>
+    <Paragraph>
+      Building a software suite
+      <a class="link" href="https://aidmedium.com?ref=chizidotdev"> @Aidmedium </a>
     </Paragraph>
 
-    <Paragraph class="">
-      Last updated on January 25, 2024. This
-      <a class="link" target="_blank" href="https://nownownow.com/about">now section</a>
+    <div class="text-muted-foreground">...</div>
+
+    <Paragraph>
+      This
+      <a class="link" target="_blank" href="https://nownownow.com/about">now page</a>
       is inspired by <a class="link" target="_blank" href="https://thu-le.com">Thu Le.</a>
     </Paragraph>
   </div>
