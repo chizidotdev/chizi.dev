4642f2b258674cd1db17e59615cfe2265b1a12dc
Chizi Victor <chiziwokoma@gmail.com>
2025-03-12 08:23:41 +0100

    feat: clock widget

---
+++ src/routes/clock.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let date = $state(new Date());
+
+  setInterval(() => {
+    date = new Date();
+  }, 1000);
+
+  const hr = $derived(date.getHours());
+  const min = $derived(date.getMinutes());
+  const sec = $derived(date.getSeconds());
+</script>
+
+<div
+  style:--hour={`calc((${hr}/12) * 360)`}
+  style:--minute={`calc((${min}/60) * 360)`}
+  style:--second={`calc((${sec}/60) * 360)`}
+  style:--width="2px"
+  class="relative flex size-12 rotate-180 items-center justify-center after:size-[5%] after:rounded-full after:bg-primary sm:size-12"
+>
+  <div class="handle h-1/4 rotate-[calc(var(--hour)*1deg)] bg-muted-foreground"></div>
+  <div class="handle h-1/3 rotate-[calc(var(--minute)*1deg)] bg-muted-foreground/80"></div>
+  <div class="handle h-2/5 rotate-[calc(var(--second)*1deg)] bg-muted-foreground/30"></div>
+</div>
+
+<style>
+  .handle {
+    position: absolute;
+    top: 50%;
+    left: calc(50%-var(--width) / 2);
+    width: var(--width);
+    transform-origin: center 0;
+  }
+</style>
--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { ThemeToggle } from "$lib/components/theme-toggle";
   import { GithubIcon, LinkedinIcon, MailIcon, TwitterIcon } from "lucide-svelte";
 
   const socials = [
@@ -14,24 +15,25 @@
   ];
 </script>
 
-<footer
-  class="my-20 flex flex-col items-center justify-between gap-12 pb-[calc(var(--clamp-nav)+2vh)] pt-10 sm:items-start"
->
-  <div class=" container flex w-fit items-center gap-3">
-    {#each socials as link}
-      <a
-        href={link.url}
-        class="inline-flex size-9 items-center justify-center rounded-full border bg-background"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={link.id}
-      >
-        <link.icon class="size-4" />
-      </a>
-    {/each}
+<footer class="container my-20 space-y-4 pt-10">
+  <div class="flex items-center justify-between">
+    <div class="flex w-fit items-center gap-3">
+      {#each socials as link}
+        <a
+          href={link.url}
+          class="inline-flex size-9 items-center justify-center rounded-full border bg-background"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={link.id}
+        >
+          <link.icon class="size-4" />
+        </a>
+      {/each}
+    </div>
+    <ThemeToggle />
   </div>
 
-  <p class=" container max-w-md text-center">
+  <p class="max-w-md text-sm">
     Developed by <span class="text-muted-foreground">@chizidotdev</span>. Built with
     <a href="https://svelte.dev/docs/kit" class="link" target="_blank" rel="noopener noreferrer">
       SvelteKit
@@ -41,7 +43,7 @@
       TailwindCSS,
     </a>
     deployed on
-    <a href="https://railway.app" class="link" target="_blank" rel="noopener noreferrer">
+    <a href="https://railway.com" class="link" target="_blank" rel="noopener noreferrer">
       Railway.
     </a>
   </p>
