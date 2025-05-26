7f9e03a80d263e1906a9ab0ee2568550eb66a0b8
Chizi Victor <chiziwokoma@gmail.com>
2025-05-26 01:29:41 +0100

    feat: code syntax highlighting

--- src/components/portable-text-code.tsx
+++
@@ -1,7 +0,0 @@
-import SyntaxHighlighter from "react-syntax-highlighter";
-
-// import { nord } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
-export function PortableTextCode({ node }: { node: any }) {
-  return <SyntaxHighlighter language={node.language}>{node.code}</SyntaxHighlighter>;
-}
