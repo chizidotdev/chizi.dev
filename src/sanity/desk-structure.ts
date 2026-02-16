8821ebe9469017af47c32fb1801d3d8cec302dde
Chizi Victor <chiziwokoma@gmail.com>
2026-02-16 11:58:59 +0100

    feat: hide site settings from desk structure

--- src/sanity/desk-structure.ts
+++ src/sanity/desk-structure.ts
@@ -4,10 +4,10 @@ export const structure = (S: StructureBuilder) =>
   S.list()
     .title("Base")
     .items([
-      S.listItem()
-        .title("Site Settings")
-        .child(S.document().schemaType("siteSettings").documentId("siteSettings")),
-      S.divider(),
+      // S.listItem()
+      //   .title("Site Settings")
+      //   .child(S.document().schemaType("siteSettings").documentId("siteSettings")),
+      // S.divider(),
       S.documentTypeListItem("project").title("Projects"),
       S.divider(),
       S.documentTypeListItem("snippet").title("Snippets"),
---
+++ src/sanity/schema-types/site-settings.ts
@@ -0,0 +1,17 @@
+export const siteSettings = {
+  name: "siteSettings",
+  title: "Site Settings",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "about",
+      title: "About",
+      type: "blockContent",
+    },
+  ],
+};
